54e1b3f3694f011d1b2717e04828986719e93f55
Diego Santos <diegosantos@gmail.com>
2022-09-28 18:24:22 -0300

    resourse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdensModule } from './ordens/ordens.module';
 
 @Module({
-  imports: [],
+  imports: [OrdensModule],
   controllers: [AppController],
   providers: [AppService],
 })
